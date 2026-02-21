ead7d833e89e990149ef6bdfa6ccb9a0d753da56
Kentaro <kenchik2004@gmail.com>
2026-02-21 11:02:48 +0900

    まとめたディスクリプタを扱うためのディスクリプタヒープを作成
    一旦1つのファイルにまとめている

---
+++ src/System/SystemUtils/DescriptorHeaps/DescriptorHeap/DescriptorHeap.cpp
@@ -0,0 +1,75 @@
+#include "DescriptorHeap.h"
+#include "System/Managers/DirectX12Manager/DirectX12Manager.h"
+
+namespace System {
+
+	DescriptorHeap::DescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE type_, unsigned int max_count)
+		:type(type_), max_num_descriptors(max_count), current_descriptors_num(0)
+	{
+		// ディスクリプタヒープの説明構造体を作成
+		D3D12_DESCRIPTOR_HEAP_DESC desc = {};
+		desc.Type = type;
+		desc.NumDescriptors = max_num_descriptors;
+		desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE; // デフォルトのフラグを使用
+		// DirectX12マネージャーからデバイスを取得して、ディスクリプタヒープを作成する
+		ID3D12Device* device = DirectX12Manager::Instance()->GetDevice();
+		if (device) {
+			device->CreateDescriptorHeap(&desc, IID_PPV_ARGS(descriptor_heap.GetAddressOf()));
+			start = descriptor_heap->GetCPUDescriptorHandleForHeapStart(); // ヒープの開始位置を取得
+			increment_size = device->GetDescriptorHandleIncrementSize(type); // ディスクリプタのサイズを取得
+		}
+	}
+	std::unique_ptr<View> CSUHeap::CreateView(const VIEW_DESC& desc, ID3D12Resource* resource)
+	{
+		std::unique_ptr<View> view = nullptr;
+		if (!resource)
+			return view;
+		auto device = DirectX12Manager::Instance()->GetDevice();
+		D3D12_CPU_DESCRIPTOR_HANDLE handle = { start.ptr + increment_size * current_descriptors_num };
+		device->CreateShaderResourceView(resource, &desc.srv_desc, handle);
+		switch (desc.type) {
+		case VIEW_DESC::SRV:
+			device->CreateShaderResourceView(resource, &desc.srv_desc, handle);
+			view = std::make_unique<ShaderResourceView>(desc.srv_desc, handle, resource);
+			break;
+		case VIEW_DESC::CBV:
+			device->CreateConstantBufferView(&desc.cbv_desc, handle);
+			break;
+		case VIEW_DESC::UAV:
+			device->CreateUnorderedAccessView(resource, nullptr, &desc.uav_desc, handle);
+			break;
+		default:
+			return view;
+		}
+		current_descriptors_num++;
+		return view;
+
+	}
+	std::unique_ptr<View> RTVHeap::CreateView(const VIEW_DESC& desc, ID3D12Resource* resource)
+	{
+
+		std::unique_ptr<View> view = nullptr;
+		if (!resource)
+			return view;
+		auto device = DirectX12Manager::Instance()->GetDevice();
+		D3D12_CPU_DESCRIPTOR_HANDLE handle = { start.ptr + increment_size * current_descriptors_num };
+		device->CreateRenderTargetView(resource, &desc.rtv_desc, handle);
+		view = std::make_unique<RenderTargetView>(desc.rtv_desc, handle, resource);
+		current_descriptors_num++;
+		return view;
+
+	}
+	std::unique_ptr<View> DSVHeap::CreateView(const VIEW_DESC& desc, ID3D12Resource* resource)
+	{
+
+		std::unique_ptr<View> view = nullptr;
+		if (!resource)
+			return view;
+		auto device = DirectX12Manager::Instance()->GetDevice();
+		D3D12_CPU_DESCRIPTOR_HANDLE handle = { start.ptr + increment_size * current_descriptors_num };
+		device->CreateDepthStencilView(resource, &desc.dsv_desc, handle);
+		view = std::make_unique<DepthStencilView>(desc.dsv_desc, handle, resource);
+		current_descriptors_num++;
+		return view;
+	}
+}
---
+++ src/System/SystemUtils/DescriptorHeaps/DescriptorHeap/DescriptorHeap.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "System/SystemUtils/Descriptors/BaseView/View.h"
+
+namespace System {
+
+
+
+
+	//-------------------------------------------------------------
+	// @brief ディスクリプタヒープ
+	// @brief D3D12のディスクリプタヒープを管理するクラス
+	//-------------------------------------------------------------
+
+
+	class DescriptorHeap
+	{
+	public:
+		DescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE type_, unsigned int max_count);
+		virtual ~DescriptorHeap() { descriptor_heap.Reset(); }
+		virtual std::unique_ptr<View> CreateView(const VIEW_DESC& desc, ID3D12Resource* resource) = 0;
+
+		// ディスクリプタヒープを管理する関数をここに追加していく
+	protected:
+		ComPtr<ID3D12DescriptorHeap> descriptor_heap;
+		D3D12_DESCRIPTOR_HEAP_TYPE type;
+		D3D12_CPU_DESCRIPTOR_HANDLE start;
+		unsigned int max_num_descriptors;
+		unsigned int current_descriptors_num;
+		unsigned int increment_size;
+	};
+
+	class CSUHeap : public DescriptorHeap
+	{
+	public:
+		CSUHeap(unsigned int max_count) :DescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, max_count) {}
+		std::unique_ptr<View> CreateView(const VIEW_DESC& desc, ID3D12Resource* resource) override;
+
+	};
+
+	class RTVHeap : public DescriptorHeap
+	{
+	public:
+		RTVHeap(unsigned int max_count) : DescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE_RTV, max_count) {}
+		std::unique_ptr<View> CreateView(const VIEW_DESC& desc, ID3D12Resource* resource) override;
+	};
+
+	class DSVHeap : public DescriptorHeap
+	{
+	public:
+		DSVHeap(unsigned int max_count) : DescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE_DSV, max_count) {}
+		std::unique_ptr<View> CreateView(const VIEW_DESC& desc, ID3D12Resource* resource) override;
+	};
+}
+
